56bd56183e1f2080cbdf3959e9f199201f8c7409
SurajKushwaha1702 <mail.surajkushwaha@gmail.com>
2021-11-25 14:54:05 +0530

    rename

---
+++ csv-file-download-api/src/main/java/com/suraj/csv/CsvApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvApplication.class, args);
+    }
+
+}
---
+++ csv-file-download-api/src/main/java/com/suraj/csv/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.suraj.csv.repository;
+
+import com.suraj.csv.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
+
